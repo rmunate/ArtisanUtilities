0e0b993151cc60fb99617257cd4e8c6cc7d9d49a
Grupo Altum <grupoaltum@MacBook-Pro-de-Grupo.local>
2023-01-04 16:50:31 -0500

    first commit

--- src/ArtisanUtilitiesServiceProvider.php
+++ src/ArtisanUtilitiesServiceProvider.php
@@ -6,6 +6,12 @@ use Illuminate\Support\ServiceProvider;
 
 class ArtisanUtilitiesServiceProvider extends ServiceProvider
 {
+
+    public function boot()
+    {
+        $this->registerCommands();
+    }
+
     protected function registerCommands()
     {
         $this->commands([
