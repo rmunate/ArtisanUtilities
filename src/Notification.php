110c715df823789dedd32205debd57bf783013ab
Grupo Altum <grupoaltum@MacBook-Pro-de-Grupo.local>
2023-03-09 13:21:13 -0500

    Fix Performance

--- src/Notification.php
+++ src/Notification.php
@@ -34,77 +34,122 @@ class Notification
     }
 
     public function getNameProject(){
-        return env('APP_NAME','Laravel');
+        return env('APP_NAME','Laravel (Actualizar Nombre en el ENV)');
+    }
+
+    public function serverName(){
+      $sn = $_SERVER["SERVER_NAME"];
+      if (str_contains($sn, 'localhost') || str_contains($sn, '127.') || str_contains($sn, '.test')) {
+        return '#';
+      }
+      return $sn;
+    }
+
+    public function serverAdress(){
+      $sa = $_SERVER["SERVER_ADDR"];
+      if (str_contains($sa, 'localhost') || str_contains($sa, '127.')) {
+        return 'Local Host (127.0.0.1)';
+      }
+      return $sa;
+    }
+
+    public function urlGit(){
+      return '';
     }
 
     public function getDataNotification(){
         return (object) [
             'fecha'     => date('Y-m-d H:i:s'),
             'branch'    => $this->branch,
-            'IP'        => $_SERVER['REMOTE_ADDR'],
+            'IP'        => $this->serverAdress(),
             'SO'        => php_uname(),
             'PHP'       => PHP_VERSION,
             'laravel'   => Application::VERSION,
-            'url'       => !empty($_SERVER["HTTP_HOST"]) ? $_SERVER["HTTP_HOST"] : 'Actualización En Ambiente Local'
+            'url'       => $this->serverName()
         ];
+    }
+
+    public function getImageHeader(){
+      return env(
+        'ARTISAN_UTILITIES_NOTIFICATION_HEADER_IMG',
+        'https://storage.googleapis.com/lola-web/storage_apls/RecursosCompartidos/au_h.png'
+      );
+    }
+
+    public function head(){
+      return '<head>
+      <meta charset="UTF-8">
+      <meta content="width=device-width, initial-scale=1" name="viewport">
+      <meta name="x-apple-disable-message-reformatting">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta content="telephone=no" name="format-detection">
+      <title>Artisan Utilities</title><!--[if (mso 16)]>
+        <style type="text/css">
+        a {text-decoration: none;}
+        </style>
+        <![endif]--><!--[if gte mso 9]><style>sup { font-size: 100% !important; }</style><![endif]--><!--[if gte mso 9]>
+    <xml>
+        <o:OfficeDocumentSettings>
+        <o:AllowPNG></o:AllowPNG>
+        <o:PixelsPerInch>96</o:PixelsPerInch>
+        </o:OfficeDocumentSettings>
+    </xml>
+    <![endif]--><!--[if !mso]><!-- -->
+      <link href="https://fonts.googleapis.com/css2?family=Imprima&display=swap" rel="stylesheet"><!--<![endif]-->
+      ' . $this->styles() . '
+     </head>';
+    }
+
+    public function styles(){
+      return '<style type="text/css">
+
+      #outlook a {
+          padding:0;
+      }
+
+      .es-button {
+          mso-style-priority:100!important;
+          text-decoration:none!important;
+      }
+
+      a[x-apple-data-detectors] {
+          color:inherit!important;
+          text-decoration:none!important;
+          font-size:inherit!important;
+          font-family:inherit!important;
+          font-weight:inherit!important;
+          line-height:inherit!important;
+      }
+
+      .es-desk-hidden {
+          display:none;
+          float:left;
+          overflow:hidden;
+          width:0;
+          max-height:0;
+          line-height:0;
+          mso-hide:all;
+      }
+
+      [data-ogsb] .es-button.es-button-1 {
+          padding:10px 5px!important;
+      }
+
+      @media only screen and (max-width:600px) {p, ul li, ol li, a { line-height:150%!important } h1, h2, h3, h1 a, h2 a, h3 a { line-height:120% } h1 { font-size:30px!important; text-align:left } h2 { font-size:24px!important; text-align:left } h3 { font-size:20px!important; text-align:left } .es-header-body h1 a, .es-content-body h1 a, .es-footer-body h1 a { font-size:30px!important; text-align:left } .es-header-body h2 a, .es-content-body h2 a, .es-footer-body h2 a { font-size:24px!important; text-align:left } .es-header-body h3 a, .es-content-body h3 a, .es-footer-body h3 a { font-size:20px!important; text-align:left } .es-menu td a { font-size:14px!important } .es-header-body p, .es-header-body ul li, .es-header-body ol li, .es-header-body a { font-size:14px!important } .es-content-body p, .es-content-body ul li, .es-content-body ol li, .es-content-body a { font-size:14px!important } .es-footer-body p, .es-footer-body ul li, .es-footer-body ol li, .es-footer-body a { font-size:14px!important } .es-infoblock p, .es-infoblock ul li, .es-infoblock ol li, .es-infoblock a { font-size:12px!important } *[class="gmail-fix"] { display:none!important } .es-m-txt-c, .es-m-txt-c h1, .es-m-txt-c h2, .es-m-txt-c h3 { text-align:center!important } .es-m-txt-r, .es-m-txt-r h1, .es-m-txt-r h2, .es-m-txt-r h3 { text-align:right!important } .es-m-txt-l, .es-m-txt-l h1, .es-m-txt-l h2, .es-m-txt-l h3 { text-align:left!important } .es-m-txt-r img, .es-m-txt-c img, .es-m-txt-l img { display:inline!important } .es-button-border { display:block!important } a.es-button, button.es-button { font-size:18px!important; display:block!important; border-right-width:0px!important; border-left-width:0px!important; border-top-width:15px!important; border-bottom-width:15px!important } .es-adaptive table, .es-left, .es-right { width:100%!important } .es-content table, .es-header table, .es-footer table, .es-content, .es-footer, .es-header { width:100%!important; max-width:600px!important } .es-adapt-td { display:block!important; width:100%!important } .adapt-img { width:100%!important; height:auto!important } .es-m-p0 { padding:0px!important } .es-m-p0r { padding-right:0px!important } .es-m-p0l { padding-left:0px!important } .es-m-p0t { padding-top:0px!important } .es-m-p0b { padding-bottom:0!important } .es-m-p20b { padding-bottom:20px!important } .es-mobile-hidden, .es-hidden { display:none!important } tr.es-desk-hidden, td.es-desk-hidden, table.es-desk-hidden { width:auto!important; overflow:visible!important; float:none!important; max-height:inherit!important; line-height:inherit!important } tr.es-desk-hidden { display:table-row!important } table.es-desk-hidden { display:table!important } td.es-desk-menu-hidden { display:table-cell!important } .es-menu td { width:1%!important } table.es-table-not-adapt, .esd-block-html table { width:auto!important } table.es-social { display:inline-block!important } table.es-social td { display:inline-block!important } .es-desk-hidden { display:table-row!important; width:auto!important; overflow:visible!important; max-height:inherit!important } }
+
+      </style>';
+    }
+
+    public function DOCTYPE(){
+      return '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+      <html xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office" style="font-family:arial, helvetica neue, helvetica, sans-serif">';
     }
 
     public function body(){
 
         $data = $this->getDataNotification();
 
-        return '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-        <html xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office" style="font-family:arial, helvetica neue, helvetica, sans-serif">
-         <head>
-          <meta charset="UTF-8">
-          <meta content="width=device-width, initial-scale=1" name="viewport">
-          <meta name="x-apple-disable-message-reformatting">
-          <meta http-equiv="X-UA-Compatible" content="IE=edge">
-          <meta content="telephone=no" name="format-detection">
-          <title>Artisan Utilities</title><!--[if (mso 16)]>
-            <style type="text/css">
-            a {text-decoration: none;}
-            </style>
-            <![endif]--><!--[if gte mso 9]><style>sup { font-size: 100% !important; }</style><![endif]--><!--[if gte mso 9]>
-        <xml>
-            <o:OfficeDocumentSettings>
-            <o:AllowPNG></o:AllowPNG>
-            <o:PixelsPerInch>96</o:PixelsPerInch>
-            </o:OfficeDocumentSettings>
-        </xml>
-        <![endif]--><!--[if !mso]><!-- -->
-          <link href="https://fonts.googleapis.com/css2?family=Imprima&display=swap" rel="stylesheet"><!--<![endif]-->
-          <style type="text/css">
-        #outlook a {
-            padding:0;
-        }
-        .es-button {
-            mso-style-priority:100!important;
-            text-decoration:none!important;
-        }
-        a[x-apple-data-detectors] {
-            color:inherit!important;
-            text-decoration:none!important;
-            font-size:inherit!important;
-            font-family:inherit!important;
-            font-weight:inherit!important;
-            line-height:inherit!important;
-        }
-        .es-desk-hidden {
-            display:none;
-            float:left;
-            overflow:hidden;
-            width:0;
-            max-height:0;
-            line-height:0;
-            mso-hide:all;
-        }
-        [data-ogsb] .es-button.es-button-1 {
-            padding:10px 5px!important;
-        }
-        @media only screen and (max-width:600px) {p, ul li, ol li, a { line-height:150%!important } h1, h2, h3, h1 a, h2 a, h3 a { line-height:120% } h1 { font-size:30px!important; text-align:left } h2 { font-size:24px!important; text-align:left } h3 { font-size:20px!important; text-align:left } .es-header-body h1 a, .es-content-body h1 a, .es-footer-body h1 a { font-size:30px!important; text-align:left } .es-header-body h2 a, .es-content-body h2 a, .es-footer-body h2 a { font-size:24px!important; text-align:left } .es-header-body h3 a, .es-content-body h3 a, .es-footer-body h3 a { font-size:20px!important; text-align:left } .es-menu td a { font-size:14px!important } .es-header-body p, .es-header-body ul li, .es-header-body ol li, .es-header-body a { font-size:14px!important } .es-content-body p, .es-content-body ul li, .es-content-body ol li, .es-content-body a { font-size:14px!important } .es-footer-body p, .es-footer-body ul li, .es-footer-body ol li, .es-footer-body a { font-size:14px!important } .es-infoblock p, .es-infoblock ul li, .es-infoblock ol li, .es-infoblock a { font-size:12px!important } *[class="gmail-fix"] { display:none!important } .es-m-txt-c, .es-m-txt-c h1, .es-m-txt-c h2, .es-m-txt-c h3 { text-align:center!important } .es-m-txt-r, .es-m-txt-r h1, .es-m-txt-r h2, .es-m-txt-r h3 { text-align:right!important } .es-m-txt-l, .es-m-txt-l h1, .es-m-txt-l h2, .es-m-txt-l h3 { text-align:left!important } .es-m-txt-r img, .es-m-txt-c img, .es-m-txt-l img { display:inline!important } .es-button-border { display:block!important } a.es-button, button.es-button { font-size:18px!important; display:block!important; border-right-width:0px!important; border-left-width:0px!important; border-top-width:15px!important; border-bottom-width:15px!important } .es-adaptive table, .es-left, .es-right { width:100%!important } .es-content table, .es-header table, .es-footer table, .es-content, .es-footer, .es-header { width:100%!important; max-width:600px!important } .es-adapt-td { display:block!important; width:100%!important } .adapt-img { width:100%!important; height:auto!important } .es-m-p0 { padding:0px!important } .es-m-p0r { padding-right:0px!important } .es-m-p0l { padding-left:0px!important } .es-m-p0t { padding-top:0px!important } .es-m-p0b { padding-bottom:0!important } .es-m-p20b { padding-bottom:20px!important } .es-mobile-hidden, .es-hidden { display:none!important } tr.es-desk-hidden, td.es-desk-hidden, table.es-desk-hidden { width:auto!important; overflow:visible!important; float:none!important; max-height:inherit!important; line-height:inherit!important } tr.es-desk-hidden { display:table-row!important } table.es-desk-hidden { display:table!important } td.es-desk-menu-hidden { display:table-cell!important } .es-menu td { width:1%!important } table.es-table-not-adapt, .esd-block-html table { width:auto!important } table.es-social { display:inline-block!important } table.es-social td { display:inline-block!important } .es-desk-hidden { display:table-row!important; width:auto!important; overflow:visible!important; max-height:inherit!important } }
-        </style>
-         </head>
+        return $this->DOCTYPE() . $this->head() . '
          <body style="width:100%;font-family:arial, helvetica neue, helvetica, sans-serif;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;padding:0;Margin:0">
           <div class="es-wrapper-color" style="background-color:#FFFFFF"><!--[if gte mso 9]>
                     <v:background xmlns:v="urn:schemas-microsoft-com:vml" fill="t">
@@ -125,7 +170,7 @@ class Notification
                           <td align="center" valign="top" style="padding:0;Margin:0;width:520px">
                            <table cellpadding="0" cellspacing="0" width="100%" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
                              <tr>
-                              <td align="left" class="es-m-txt-c" style="padding:0;Margin:0;font-size:0px"><a target="_blank" href="#" style="-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;text-decoration:underline;color:#2D3142;font-size:18px"><img src="https://storage.googleapis.com/lola-web/storage_apls/RecursosCompartidos/au_h.png" alt="Confirm email" style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic;border-radius:100px" width="100" title="Confirm email"></a></td>
+                              <td align="left" class="es-m-txt-c" style="padding:0;Margin:0;font-size:0px"><a target="_blank" href="#" style="-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;text-decoration:underline;color:#2D3142;font-size:18px"><img src="' . $this->getImageHeader() . '" alt="Artisan Utilities" style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic;border-radius:100px" width="100" title="Artisan Utilities"></a></td>
                              </tr>
                            </table></td>
                          </tr>
